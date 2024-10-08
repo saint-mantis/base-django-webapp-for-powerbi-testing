574954a171e5217a4743399bf60312b786f4b284
Arun Babu <59362586+saint-mantis@users.noreply.github.com>
2024-10-08 15:51:43 +0530

    initial

---
+++ powerbitestapp/ap/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+
+]
+
+
